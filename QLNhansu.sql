9cc3d69181c036d94665febfff875f6f8c041740
bonnmh1703 <bonnmh1703@gmail.com>
2018-11-24 09:34:52 +0700

    dulieu

--- QLNhansu.sql
+++ QLNhansu.sql
@@ -1,4 +1,4 @@
----DROP DATABASE QLNhansu
+﻿----DROP DATABASE QLNhansu
 CREATE DATABASE QLNhansu
 GO
 USE QLNhansu
@@ -26,3 +26,38 @@ CREATE TABLE BangChamCong(
 	Luongthang float,
 	constraint fk1_PhongBan foreign key(MaNV) references NhanVien(MaNV)
 )
+go
+INSERT INTO PHONGBAN
+VALUES ( N'Kế toán')
+go
+INSERT INTO PHONGBAN
+VALUES ( N'Tài chính')
+go
+INSERT INTO PHONGBAN
+VALUES ( N'Quản lý')
+GO
+INSERT INTO NhanVien
+VALUES ( N'NGUYỄN MINH HIẾU BỐN' , N'ấp 3 Tân ÂN Cần Đước Long An' , 1 , 5000000)
+
+GO
+INSERT INTO NhanVien
+VALUES ( N'NGUYỄN VĂN ANH QUỐC' , N'Tân Phú' , 2 , 6000000)
+GO
+INSERT INTO NhanVien
+VALUES ( N'NGUYỄN HOÀI THANH' , N'Đồng Xoài' , 3 , 5000000)
+GO
+INSERT INTO NhanVien
+VALUES ( N'TRỊNH HOÀNG HUY' , N'Tokyo JAPAN' , 1 , 7000000)
+
+go
+insert into BangChamCong
+values (2016 , 1 , 1 ,20 , 4545454 )
+go
+insert into BangChamCong
+values (2016 , 1 , 2 ,19 , 5181818 )
+go
+insert into BangChamCong
+values (2016 , 1 , 3 ,22 , 7000000 )
+go
+insert into BangChamCong
+values (2016 , 1 , 4 ,17 , 3863863 )
