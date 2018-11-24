816eb8512271901549500ae8618b62a5a6b1ffbd
huyhoang1905 <moneracvt1905@gmail.com>
2018-11-24 09:04:59 +0700

    QSNL

---
+++ QLNS.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE QLNhansu
+GO
+USE QLNhansu
+GO
+CREATE TABLE PhongBan (
+    MaPB  INT IDENTITY(1,1)  PRIMARY KEY ,
+   TenPB  NVARCHAR(30) NOT NULL
+)
+GO
+CREATE TABLE NhanVien (
+    MaNV  int IDENTITY PRIMARY KEY ,
+   Hoten  NVARCHAR(40) NOT NULL,
+   Diachi nvarchar(255),
+   MaPB int,
+   LuongCB float,
+   constraint fk_PhongBan foreign key(MaPB) references PhongBan(MaPB)
+)
+GO
+CREATE TABLE BangChamCong(
+	Nam int,
+	Thang int,
+	MaNV int ,
+	Songay int,
+	Luongthang float,
+	constraint fk1_PhongBan foreign key(MaNV) references NhanVien(MaNV)
+)
